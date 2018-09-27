48da004f23f08b82618f3ac9c7938c54f6c473ae
JERONIMO <JERONIMO@JERONIMO-S>
2018-09-26 22:40:02 -0300

    nada.

---
+++ sol-engine/src/main/java/experiment/runners/SmartRunner.java
@@ -0,0 +1,32 @@
+package experiment.runners;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+
+	public static void getInput() {
+		List<List<String>> baseResults = new ArrayList<List<String>>();
+		
+		List<String> aResult = new ArrayList<String>();
+		aResult.add("http://dbpedia.org/resource/Miguasha_Group");
+		baseResults.add(aResult);
+		
+		List<String> bResult = new ArrayList<String>();
+		bResult.add("http://dbpedia.org/resource/Miguasha_Group");
+		baseResults.add(bResult);
+		
+		List<String> cResult = new ArrayList<String>();
+		cResult.add("http://dbpedia.org/resource/Miguasha_Group");
+		baseResults.add(cResult);
+
+	}
+
+}
